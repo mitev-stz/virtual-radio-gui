4239c29b98e55e77ca894e7ba98aeb52e63aff59
Yordan Mitev <darkyto99@gmail.com>
2020-05-11 11:00:40 +0200

    Updates

---
+++ precache-manifest.f2efe4ed48381bae6290bb94065bf72c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c79135a6a517d5064282fcb01499d9",
+    "url": "/virtual-radio-gui/index.html"
+  },
+  {
+    "revision": "d4284271b7f05f81cb68",
+    "url": "/virtual-radio-gui/static/css/main.3d3f11d6.chunk.css"
+  },
+  {
+    "revision": "be4109e5eba147dbc2f7",
+    "url": "/virtual-radio-gui/static/js/2.58154a02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/virtual-radio-gui/static/js/2.58154a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4284271b7f05f81cb68",
+    "url": "/virtual-radio-gui/static/js/main.c93e684e.chunk.js"
+  },
+  {
+    "revision": "53686d8caf58bb348dfa",
+    "url": "/virtual-radio-gui/static/js/runtime-main.337f05ae.js"
+  }
+]);
