714547ff576ba1ebd232fb2d58d0fc2e048086ed
Yordan Mitev <darkyto99@gmail.com>
2020-05-17 12:30:07 +0200

    added axios dependency

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './assets/styles/home.css';
 
-function Home() {
-  return (
-    <div className="Home">
-      <div className="test">
-        Hello World! Entry!
+class Home extends React.Component {
+  componentDidMount(){
+    const resp = fetch("https://radio.ethylomat.de/api/v1/channels");
+    console.log(resp);
+  }
+  render() {
+    return (
+      <div className="Home">
+        <div className="test">
+          Hello World! Entry!
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
