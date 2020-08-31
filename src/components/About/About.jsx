73d0f234fbb5fc68a712be70621b166580c31354
Yordan Mitev <darkyto99@gmail.com>
2020-08-31 04:50:44 +0200

    new content in home and about pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import "./assets/styles/about.css";
+import pic from "../../assets/img/profile_pic.png";
+import aboutUs from "../../assets/img/about_us.png";
 
 const About = () =>(
-  <div className="centered-container aboutpage bordered-component">
-    <div className="centered-context">
-       This is the About page.
+  <div className="about">
+    <div className="about-us-intro">
+      <span>A little bit</span>
+      <img src={aboutUs} alt="About us"></img>
     </div>
+
+    <section className="section-1 section section-content padding-left-1">
+      Hey there! Christopher here! I'm from Germany and I am a computer science student at the university of Heidelberg, Germany.
+      Although I had some difficulties in the beginning, I have been working hard on the hardware radio in order to make it pretty and useful again.
+      And.. turns out it's payed out!
+      <img src={pic} alt="hello"></img>
+    </section>
+    <section className="section-2 section section-content  padding-left-1">
+       Hi, my name is Yordan and I'm from Bulgaria. I was born in Stara Zagora, a city right in the middle of the land.
+       Right now I'm studying mathematics at the university of Heidelberg, Germany. I strive for a bachelor degree and I'm half way there.
+       I'm participating in this project as part of my software praktikum. I was responsible for the development of the virtual radio frontend.
+       My interests involve web development, regression and interpolation as well as russian literature. I am also an active chess player.
+
+       <img src={pic} alt="hello"></img>
+    </section>
+    <section className="section-3 section section-content padding-left-1">
+       Good day from me as well! I am Lennart and I'm also part of the project.
+       I'm from Germany and I'm studying computer science in the same university as the others. I developed the backend side of the project and I'm happy to see the results of the work we put in.
+        I would love to see that the new generation appreciates it too, especially the hardware radio.
+       <img src={pic} alt="hello"></img>
+    </section>
   </div>
 
 );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ class Home extends React.Component {
             </div>
           </div>
           <section className="home-info-content">
-            <div>
+            <div className="padding-left-1">
               Our project consists of building a client which delivers informative material for the history of computer science to the new generations.
               We split up our project in 3 parts and we have now 2 clients and a backend application.
               A real old radio and a virtual radio which mimics it, broadcast different speeches regarding computer science at different frequencies.
@@ -32,7 +32,11 @@ class Home extends React.Component {
           <div className="section-title">The Hardware Radio</div>
           <div className="section-content">
               <div className="section-intro">
-                This is some content.
+                The very old hardware is controlled by a raspberry pi computer. Assets that we use are an a/d converter and a potentiometer.
+                After the order of the needed hardware we did some planning for the development process.
+                 We did remote development using a laptop running pycharm which deploys code to SFTP server. The code is then executed there and the output is on terminal
+                Software part – We used ssh for configuring rasbian lite and created flowcharts(PAP) for the py software.
+                A big difficulty was definitely creating a valid circuit diagram.
               </div>
 
               <div className="hardware-radio-img">
@@ -77,11 +81,16 @@ class Home extends React.Component {
 
                 <div className="challenges-clickable">
                   <span className="clickable"  onClick={this.clickableOnClick}>Challenges on the road</span>
-                  <div className="hidden"> challenes </div>
+                  <div className="hidden">
+                    The delivery of the needed items was postponed for a couple of weeks and at first we had to do more of a planning than writing stuff.
+                    Another challenge was to find a clever way to install a potentiometer.
+                  </div>
                 </div>
                 <div className="purpose-clickable">
                   <span className="clickable"  onClick={this.clickableOnClick}>Purpose</span>
-                  <div className="hidden"> purpose</div>
+                  <div className="hidden">
+                    The old radio will be placed in the school area, where all the students get to use it freely for studying computer science.
+                  </div>
                 </div>
               </div>
           </div>
@@ -90,7 +99,12 @@ class Home extends React.Component {
           <div className="section-title">The Virtual Radio</div>
           <div className="section-content">
               <div className="section-intro">
-                Some content here...
+                The virtual radio frontend was implemented using React and Bootstrap.
+                Pipeline consists of 3 stages - build, test, deploy.
+                We used heroku cloud platform for hosting the production build.
+                The focus at the start of the project was to implement the basic functionality, the first working prototype and demo was on 13-07-20.
+                At the end of July and in the beginning of August we developed a new radio design which was more consistent and easier to use.
+                In the following weeks we did some implementation improvements and developed the information pages for the project.
               </div>
 
               <div className="virtual-radio-img">
@@ -134,11 +148,19 @@ class Home extends React.Component {
                 </div>
                 <div className="challenges-clickable">
                   <span className="clickable"  onClick={this.clickableOnClick}>Challenges on the road</span>
-                  <div className="hidden"> challenges</div>
+                  <div className="hidden">
+                     One of the biggest challenges on the road was definitely a good looking design model
+                     which not only mimics the look and feel of the real old radio, but also assures a
+                     good user experience when interacting with it. Figuring out the noise in between channels was also quite a trouble.
+                   </div>
                 </div>
                 <div className="purpose-clickable">
                   <span className="clickable" onClick={this.clickableOnClick}>Purpose</span>
-                  <div className="hidden"> purposee</div>
+                  <div className="hidden">
+                    The web is definitely one of the most comfortable placed to do your further studies once
+                    you get involved in computer science, and so the virtual radio provides the students who came across
+                    our hardware radio another place to do their studies.
+                  </div>
                 </div>
               </div>
           </div>
@@ -147,7 +169,14 @@ class Home extends React.Component {
           <div className="section-title">The Server Backend</div>
           <div className="section-content">
               <div className="section-intro">
-                Some content here...
+                The backend serves audio files and channel info we want to host.
+                Implementation using python django and djando admin framework for editing hosted data.
+                We used Swagger for the api documentation.
+                Pipeline consists of 3 stages - build, test, deploy
+                There are 3 docker containers for 3 services that the backend provides – backend app, nginx service(routing and  serving static files), postgres for managing data
+                The first working api was on 18-05-20.
+                The first created tests had 85% coverage.
+                There is also an automatic convertion of filetypes.
               </div>
 
               <div className="virtual-radio-img">
@@ -191,11 +220,11 @@ class Home extends React.Component {
                 </div>
                 <div className="challenges-clickable">
                   <span className="clickable"  onClick={this.clickableOnClick}> Challenges on the road</span>
-                  <div className="hidden"> challenges</div>
+                  <div className="hidden"> Setting up nginx service for routing and serving static files, implementing CI/CD pipeline.</div>
                 </div>
                 <div className="purpose-clickable">
                   <span className="clickable"  onClick={this.clickableOnClick}>Purpose</span>
-                  <div className="hidden"> purpose</div>
+                  <div className="hidden"> This API provides both of the clients with the needed audio files and gives them some metadata in regards as well. There is an admin interface so one could easily add, remove of change the audio files.</div>
                 </div>
               </div>
           </div>
